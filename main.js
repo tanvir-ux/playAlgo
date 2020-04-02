d77f3ec910175a61f2ebd77c4e54ecadcf3ed926
tanvir-ux <tanviralamshuvo@gmail.com>
2020-04-02 17:24:13 +0600

    inial commit 2020 april

---
+++ main.js
@@ -0,0 +1,43 @@
+let el = document.createElement("div");
+
+el.textContent = "Hello World";
+el.style.fontSize = "40px";
+el.style.padding = "0 20px";
+
+let body = document.querySelector("body");
+body.appendChild(el);
+
+let button = document.createElement("button");
+button.textContent = "New Item";
+button.style.width = "200px";
+button.style.height = "30px";
+button.style.margin = "0 0 0 20px";
+body.appendChild(button);
+
+button.addEventListener("click", addItem);
+
+let removeButton = document.createElement("button");
+removeButton.textContent = "Remove Item";
+removeButton.style.width = "200px";
+removeButton.style.height = "30px";
+removeButton.style.margin = "0 0 0 100px";
+body.appendChild(removeButton);
+
+button.addEventListener("click", removeItem);
+
+const items = []
+
+function display(){
+    console.log("I will display if you click.");
+}
+
+function addItem(){
+    let newItem = document.createElement("div");
+    newItem.textContent = "New Item";
+    newItem.style.padding = "0 20px";
+    body.appendChild(newItem);
+}
+
+function removeItem(){
+    body.removeChild(newItem);
+}
