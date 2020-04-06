7eb416b5e94a0f8b748f7ece5b5229de44e9a064
tanvir-ux <tanviralamshuvo@gmail.com>
2020-04-06 14:52:43 +0600

    mod of numbers

--- scrappy.py
+++ scrappy.py
@@ -7,3 +7,11 @@ greeting = "This is my new welcome note"
 
 def helloWorld(greeting):
     print(greeting)
+
+
+numbers = [1, 2, 3, 4, 5]
+for x in numbers:
+    if(x % 2 == 0):
+        print numbers[x]
+
+
