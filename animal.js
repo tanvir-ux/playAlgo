4833e901cb5676b71bfc6a9ce4f43fedf0927b4d
tanvir-ux <tanviralamshuvo@gmail.com>
2020-04-10 17:57:45 +0600

    this is coming from the master

--- animal.js
+++ animal.js
@@ -9,5 +9,7 @@ class Animal {
  	console.log("ok this is something else");
 	console.log("ok this is something after changing the font size");
 	console.log("we are doing good, day by day");
+	console.log("Now, I want something more interesting");
+
 
 }
