264053bbd0c2b38a6b5b1608044d8d1c067d23d8
tanvir-ux <tanviralamshuvo@gmail.com>
2020-04-10 17:48:06 +0600

    ok this is fine

--- animal.js
+++ animal.js
@@ -4,7 +4,10 @@ class Animal {
     constructor(name){
         this.name = name;
     }
-
-
+	
+	console.log("This is something from the command line vim editor");
+ 	console.log("ok this is something else");
+	console.log("ok this is something after changing the font size");
+	console.log("we are doing good, day by day");
 
 }
